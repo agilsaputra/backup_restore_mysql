7963c055ada7da51ccfa4c3dd3ba63772f78e14c
agilsaputra <m.agil.saputra@gmail.com>
2023-08-18 03:13:23 +0700

    push

--- backupmysql/backup.py
+++ backupmysql/backup.py
@@ -4,12 +4,10 @@ import datetime
 import subprocess
 
 # MySQL database details
-db_user = 'admin1'
-db_password = '818642'
 db_name = 'ingest_data_terminal'
 
 # Backup directory
-backup_dir = '/home/gandalf/DataEngineer/PORTFOLIO_SQL/backupsql/'
+backup_dir = '/home/gandalf/DataEngineer/PORTFOLIO-SQL/backupsql/'
 
 # Current date and time
 timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')
@@ -18,7 +16,7 @@ timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')
 backup_filename = f'{db_name}_backup_{timestamp}.sql'
 
 # MySQL dump command
-mysql_dump_cmd = f'mysqldump -u {db_user} -p{db_password} {db_name} > {backup_dir}{backup_filename}'
+mysql_dump_cmd = f'mysqldump --defaults-extra-file=mysql_config.cnf {db_name} > {backup_dir}{backup_filename}'
 
 # Run the MySQL dump command
 subprocess.call(mysql_dump_cmd, shell=True)
